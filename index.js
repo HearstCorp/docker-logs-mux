3665662db0eadb2dba49edd1fe1f491d48cd2330
Michael Hernandez <michael.hernandez1988@gmail.com>
2016-05-11 11:52:03 -0400

    More debug info, and remove stream on end

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ var registerContainerStream = function () {
           console.error('docker-logs-mux:', 'we could not process the stream for', container.Id, container.Names);
           return;
         }
-        if (streams[containerInfo.Id] !== undefined) return;
-        
+        if (streams[containerInfo.Id] !== undefined) {
+          return;
+        }
+        console.log('docker-logs-mux:', 'Adding stream for', containerInfo.Id, containerInfo.Names)
         stream.on('data', function (buffer) {
           if(containerInfo.Id.substr(0,12) !== process.env.HOSTNAME) {
             var str = containerInfo.Id.substr(0,12) + ' - ' + containerInfo.Names + ' : ' + buffer.toString('utf8');
@@ -21,14 +23,16 @@ var registerContainerStream = function () {
           }
         });
         stream.on('end', function () {
-          console.log('docker-logs-mux:', 'Stream for', containerInfo.Id, 'has ended.');
+          console.log('docker-logs-mux:', 'Stream for', containerInfo.Id, containerInfo.Names, 'has ended.');
+          stream.close();
+          streams[containerInfo.Id] = undefined;
         })
         stream.on('close', function () {
-          console.log('docker-logs-mux:', 'Stream for', containerInfo.Id, 'has closed.');
+          console.log('docker-logs-mux:', 'Stream for', containerInfo.Id, containerInfo.Names, 'has closed.');
           streams[containerInfo.Id] = undefined;
         });
         stream.on('error', function () {
-          console.log('docker-logs-mux:', 'Stream for', containerInfo.Id, 'has errored.');
+          console.log('docker-logs-mux:', 'Stream for', containerInfo.Id, containerInfo.Names, 'has errored.');
           streams[containerInfo.Id] = undefined;
         });
         
